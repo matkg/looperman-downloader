7630b2293000ebfd0af416bea18bfafc66d7dd1f
maklug <maklug@uni-bremen.de>
2021-08-19 23:06:18 +0200

    move to next page

--- looperman_downloader.py
+++ looperman_downloader.py
@@ -3,6 +3,9 @@ from link2mp3 import get_mp3_link
 from bs4 import BeautifulSoup
 import requests
 import sys
+import re
+
+LOOP_DETAIL_REGEX = ".*loops/detail/.*/.*"
 
 class Looperman_Downloader:
     amount = 0
@@ -14,23 +17,41 @@ class Looperman_Downloader:
         self.data = []
         self.url = url
         self.amount = amount
+        self.download()
 
     def download(self):
+        #while len(self.links) != self.amount:
         self.move_to_page()
         self.get_song_links_of_page()
+        self.move_to_next_page()
 
     def get_song_links_of_page(self):
         for link in self.soup.findAll('a'):
+
+            if len(self.links) == self.amount:
+                break
+
             href = link.get('href')
-            if "loops/detail/" in href and href not in self.links:
+            is_a_song = bool(re.match(LOOP_DETAIL_REGEX,href))
+            if is_a_song and href not in self.links:
                 self.links.append(href)
-        
-        print(self.links)
 
     def move_to_page(self):
         r = requests.get(self.url)
         self.soup = BeautifulSoup(r.text, features="html.parser")
 
+    def move_to_next_page(self):
+        page_equald_idx = self.url.find("=") 
+        page_number = ""
+        number_idx = page_equald_idx + 1
+
+        while self.url[number_idx].isdigit():
+            page_number += self.url[number_idx]
+            number_idx += 1
+
+        # replace page number with next one
+        self.url = re.sub("page=\d*", "page="+str(int(page_number)+1), self.url)
+    
 def main():
     url = sys.argv[1]
     amount = sys.argv[2]
