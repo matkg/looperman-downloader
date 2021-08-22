d5fac1a6b2dea641c24955c455dfd76bd252bde1
maklug <maklug@uni-bremen.de>
2021-08-22 23:01:22 +0200

    24h download limit check

--- file_utils.py
+++ file_utils.py
@@ -4,6 +4,8 @@ import errno
 from time import sleep
 import requests
 
+LIMIT_REACHED = "Sorry, due to high demand, download limits are in place and you have reached the limit.<br> Try again in 24 hours."
+
 def download_file(session, url, filename, file_end):
     filename = filename+file_end
     
@@ -15,13 +17,14 @@ def download_file(session, url, filename, file_end):
     print(f"Download file {filename}")
 
     with session.get(url, stream=True) as r:
-        if r.status_code is not requests.codes.ok:
-            print(r.json())
+        if r.text == LIMIT_REACHED:
+            print("Download limit reached! Abort Downloading")
+            exit()
         
         with open(filename, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
-    sleep(5) # necessary because of download limit
+    sleep(6) # necessary because of download limit
 
 def create_dirs(filename):
     if not os.path.exists(os.path.dirname(filename)):
